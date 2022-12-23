0e5d1fc0f4b6f1086e8ea823e35365c5166cc1b2
Ednlpz <e.lopez103199@gmail.com>
2022-12-23 14:19:59 +0800

    Updated event list component
    Event list component implementation
    Event list test
    Updated view events page for display events
    Updated event entry to have an optional id property
    src/test-constants directory created - for mocking event value

--- src/features/events-list/index.tsx
+++ src/features/events-list/index.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+// import { getAllRecord } from "@utils/indexedDb";
+import { getAllRecord } from "../../utils/indexedDb";
+import { useEffect, useState } from "react";
+import Event from "./event";
+
+interface EventList {
+  events: EventEntryList;
+}
 
 const EventsList = () => {
-  return <div>EventsList</div>;
+  const [events, setEvents] = useState<null | EventEntryList>(null);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const events = await getAllRecord();
+      setEvents(events);
+    };
+    fetchEvents();
+  }, []);
+
+  if (!events) {
+    return <p>Loading...</p>;
+  }
+  return (
+    <>
+      {events?.map((event: EventEntry) => (
+        <Event key={event.id} eventInformation={event} />
+      ))}
+    </>
+  );
 };
 
 export default EventsList;
